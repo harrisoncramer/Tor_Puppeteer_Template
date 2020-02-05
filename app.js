f8befc47214ed33794bb2ea21776e9a5fe96e0d1
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2020-02-05 13:15:30 -0500

    added work script

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const logger = require("./logger");
 const { setupPuppeteer, setPageBlockers, setPageScripts } = require("./setup");
-const work = async (page) => {
+
+const getLinks = async (page) => {
   logger.info(`Performing work...`);
+  await setPageScripts(page);
+  let links = await page.evaluate(_ => {
+    let urlChecker = new RegExp("^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$");
+    let links = makeArrayFromDocument("a")
+      .filter(x => x.href.match(urlChecker)) // Get rid of non-links...
+      .map(x => new URL(x.href)) // Turn into URLs..
+      .filter(x => x.host === "www.nationaljournal.com" && x.href !== window.location.href) // Where host is the same, but not entire URL.
+      .reduce((agg, item) => agg.some(x => x.href === item.href) ? agg : [ ...agg, item ], []); // Remove duplicates...
+    return JSON.stringify(links);
+  });
+  return links;
 };
 
 const scrape = async (url) => {
@@ -11,9 +23,10 @@ const scrape = async (url) => {
 
   try {
     await setPageBlockers(page);
-    await setPageScripts(page);
     await page.goto(url, 0);
-    await work(page);
+    let stringLinks = await getLinks(page);
+    let links = JSON.parse(stringLinks);
+    console.log(links.length, links);
   } catch (err) {
     logger.error(`There was an error with the bot: `, err);
     process.exit(1);
